52d78516500358f0a9e611c2e65216ae3c674d13
Mukesh Yadav <mukesh@chefling.net>
2018-07-03 14:46:44 +0530

    Example added

---
+++ Example/SwipePopNavigationDemo/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  SwipePopNavigationDemo
+//
+//  Created by Mukesh on 03/07/18.
+//  Copyright © 2018 BooEat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
