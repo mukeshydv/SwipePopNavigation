567ebfbacb58bdefb0ec4d92b66ae74bf916186c
Mukesh Yadav <mukesh@chefling.net>
2018-07-03 12:50:23 +0530

    Initial Commit

---
+++ SwipePopNavigation/SwipePopNavigation.h
@@ -0,0 +1,19 @@
+//
+//  SwipePopNavigation.h
+//  SwipePopNavigation
+//
+//  Created by Mukesh on 03/07/18.
+//  Copyright © 2018 BooEat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipePopNavigation.
+FOUNDATION_EXPORT double SwipePopNavigationVersionNumber;
+
+//! Project version string for SwipePopNavigation.
+FOUNDATION_EXPORT const unsigned char SwipePopNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipePopNavigation/PublicHeader.h>
+
+
